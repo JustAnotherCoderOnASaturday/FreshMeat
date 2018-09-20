b1c89a2e278c27ceb66443cd6c849a9106537eba
Yoko Zoko <kawaiieraser@protonmail.com>
2018-09-19 22:31:36 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/reads')
+def reads():
+    """Documentations that I have written, like reverse engi, or instructions"""
+    return render_template('Writing.html')
+
+
+@app.route('/projects')
+def fun():
+    """Prankware downloads and stuff"""
+    return render_template('projects.html')
+
+
+@app.route('/experiments')
+def experiment():
+    """You Shouldn't be on this page my dude"""
+    return 'You Shouldnt be here Sir.'
+
+
+if __name__ == '__main__':
+    """This Project Was created Sept. 9 2018"""
+    app.run()
